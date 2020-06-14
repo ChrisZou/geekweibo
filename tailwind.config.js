86b6814de18dd759f43b1ffbb4cf62f55c18b40c
ZouYong <zouyong@bytedance.com>
2020-06-14 17:09:47 +0800

    feat: tailwindui and alphinejs

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui')
+  ]
+}
