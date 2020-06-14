d08584682588d4de393267ee9be3d103c6cddf4b
ZouYong <zouyong@bytedance.com>
2020-06-14 20:24:42 +0800

    feat: adding tweet from tweet/index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.order(created_at: :desc)
   end
 
   # GET /tweets/1
