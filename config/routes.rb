dd7401b66510f989362ccb1b562873a7fb304f67
ZouYong <zouyong@bytedance.com>
2020-06-24 21:45:30 +0800

    feat: limit user resources to only show, edit and update action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :tweets
-  resources :users
+  resources :users, only: %i[show edit update]
   root to: "tweets#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
