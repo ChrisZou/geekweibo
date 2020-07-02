5df0e40fa6ae5c679a2e8764cba3a82ad78f5abd
ZouYong <zouyong@bytedance.com>
2020-07-02 18:51:50 +0800

    feat: auto refresh browser when erb file changes

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+// Auto refresh browser when erb file changes
+const chokidar = require('chokidar')
+environment.config.devServer.before = (app, server) => {
+  chokidar.watch(['app/views/**/*.erb']).on('change', () => server.sockWrite(server.sockets, 'content-changed'))
+}
+
 module.exports = environment.toWebpackConfig()
