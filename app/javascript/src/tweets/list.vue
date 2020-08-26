c51342e24087ca53f85fa8287b5118762d5bae13
ChrisZou <zoumengdie@gmail.com>
2020-08-27 01:20:33 +0800

    feat: delete comment

--- app/javascript/src/tweets/list.vue
+++ app/javascript/src/tweets/list.vue
@@ -55,11 +55,9 @@
               d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
             />
           </svg>
-          <span
-            class="inline-block ml-1 text-xs"
-            v-if="tweet.comments.length"
-            >{{ tweet.comments.length }}</span
-          >
+          <span class="inline-block ml-1 text-xs" v-if="tweet.comment_count">{{
+            tweet.comment_count
+          }}</span>
           <svg
             viewBox="0 0 24 24"
             class="w-4 h-4 ml-4 fill-current"
@@ -108,14 +106,34 @@
               class="object-cover w-8 h-8 rounded-full"
             />
             <div class="w-full ml-2">
-              <div class="text-xs font-medium text-gray-900">
-                {{ comment.user.nickname }}
+              <div class="flex items-center justify-between">
+                <div class="text-xs font-medium text-gray-900">
+                  {{ comment.user.nickname }}
+                </div>
+                <svg
+                  class="w-5 h-5 p-1 text-gray-400 rounded-full hover:bg-gray-200"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  @click="confirmDeleteComment(tweet, comment)"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
               </div>
               <div
                 class="text-sm text-gray-500 markdown"
                 v-html="markdown(comment.body)"
               ></div>
             </div>
+          </div>
+          <div
+            v-if="tweet.has_more_comment"
+            class="pt-3 mt-3 text-sm text-center border-t border-gray-200"
+          >
+            查看更多
           </div>
         </div>
       </div>
@@ -125,7 +143,6 @@
 </template>
 
 <script>
-import DropdownMenu from "@innologica/vue-dropdown-menu";
 import { VTooltip, VPopover, VClosePopover } from "v-tooltip";
 
 const marked = require("marked");
@@ -139,11 +156,14 @@ marked.setOptions({
 });
 
 export default {
-  components: { "dropdown-menu": DropdownMenu, "v-popover": VPopover },
+  components: { "v-popover": VPopover },
   props: ["tweets", "dialogShower"],
   data() {
     this.tweets.forEach(t => {
       t.showMenu = false;
+      t.comment_count = t.comments.length;
+      t.has_more_comment = t.comments.length > 5;
+      t.comments = t.comments.slice(0, 5);
     });
     return {
       items: this.tweets,
@@ -224,6 +244,32 @@ export default {
               this.$modal.hide("dialog");
               window.delete(`/tweets/${tweet.id}`).then(res => {
                 this.items = this.items.filter(t => t.id != tweet.id);
+              });
+            }
+          }
+        ]
+      });
+    },
+    confirmDeleteComment(tweet, comment) {
+      this.$modal.show("dialog", {
+        title: "确定删除这条评论？",
+        text: "此操作无法撤销",
+        buttons: [
+          {
+            title: "取消",
+            class: "focus:outline-none py-3 hover:bg-gray-100 ",
+            handler: () => {
+              this.$modal.hide("dialog");
+            }
+          },
+          {
+            title: "确定",
+            default: true,
+            class: "focus:outline-none py-3 hover:bg-gray-100",
+            handler: () => {
+              this.$modal.hide("dialog");
+              window.delete(`/comments/${comment.id}`).then(res => {
+                tweet.comments = tweet.comments.filter(c => c.id != comment.id);
               });
             }
           }
