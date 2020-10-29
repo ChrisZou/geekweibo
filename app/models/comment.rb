044220eaa7edaf6f2d13f26c5e7f7d22df58caf1
ChrisZou <zoumengdie@gmail.com>
2020-10-30 00:11:38 +0800

    fix: make parent_comment optional

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
-  belongs_to :parent_comment, class_name: 'Comment'
+  belongs_to :parent_comment, class_name: 'Comment', optional: true
   has_many :subcomments, class_name: 'Comment', foreign_key: 'parent_comment_id'
 end
