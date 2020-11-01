0c0278e839243b1280fcc9f73582274ec842f839
ChrisZou <zoumengdie@gmail.com>
2020-11-01 17:42:16 +0800

    feat: use TweetOperation for tweet/show operation and enable shareTweet

--- app/javascript/src/tweets/Detail.vue
+++ app/javascript/src/tweets/Detail.vue
@@ -1,54 +1,28 @@
-  <template>
+<template>
   <div>
     <div class="flex flex-row mb-4 bg-white shadow sm:rounded-lg">
-      <img :src="tweet.user.avatar" class="inline-block object-cover w-12 h-12 mt-4 ml-4 rounded-full" />
+      <img :src="scaledAvatar(tweet.user.avatar, tweet.user.nickname)" class="inline-block object-cover w-12 h-12 mt-4 ml-4 rounded-full" />
       <div class="w-full p-4 overflow-x-auto">
-        <h3 class="text-lg font-medium text-gray-900 leading-6">
-          {{ tweet.user.nickname }}
-        </h3>
+        <h3 class="text-lg font-medium text-gray-900 leading-6">{{ tweet.user.nickname }}</h3>
         <div class="mt-1 text-sm text-gray-500 leading-5 markdown" v-html="markdown(tweet.body)"></div>
-        <div class="flex items-center justify-end mt-4">
-          <svg fill="none" class="w-4 h-4" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-            <path
-              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-            ></path>
-          </svg>
-          <span class="inline-block ml-1 text-xs" v-if="tweet.comments.length">{{ tweet.comments.length }}</span>
-          <svg
-            viewBox="0 0 24 24"
-            class="w-4 h-4 ml-4 fill-current"
-            :class="tweet.liked ? 'text-red-500' : 'text-black'"
-            @click.stop="toggleLike(tweet)"
-          >
-            <path
-              class="heroicon-ui"
-              d="M12.76 3.76a6 6 0 018.48 8.48l-8.53 8.54a1 1 0 01-1.42 0l-8.53-8.54a6 6 0 018.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 10-5.66-5.66l-1.46 1.47a1 1 0 01-1.42 0L9.83 5.17a4 4 0 10-5.66 5.66L12 18.66l7.83-7.83z"
-            />
-          </svg>
-          <span v-if="tweet.like_count" class="inline-block ml-1 text-xs">{{ tweet.like_count }}</span>
-        </div>
+        <TweetOperation class="justify-end mt-8" :tweet="tweet" :currentUser="currentUser" :showTweetComment="noop" :shareTweet="shareTweet" />
         <CommentList :currentUser="currentUser" :tweet="tweet" />
       </div>
     </div>
+    <modal name="share-tweet" width="300px" height="auto" background="none">
+      <TweetSharer :sharing_tweet="tweet" />
+    </modal>
   </div>
 </template>
 <script>
-import DOMPurify from 'dompurify'
-const marked = require('marked')
-marked.setOptions({
-  renderer: new marked.Renderer(),
-  gfm: true,
-  breaks: true,
-  pedantic: false,
-  smartLists: true,
-  smartypants: false,
-})
-
 import LoginDialog from '../common/LoginDialog.vue'
 import CommentList from './comments/CommentList'
+import TweetSharer from './TweetSharer.vue'
+import TweetOperation from './TweetOperation'
+import { markdown, scaledAvatar } from '../common/Utils'
 
 export default {
-  components: { CommentList },
+  components: { CommentList, TweetOperation, TweetSharer },
   props: ['origin_tweet'],
   data() {
     return {
@@ -58,28 +32,14 @@ export default {
     }
   },
   methods: {
-    toggleLike() {
-      if (!this.currentUser) {
-        this.showLoginDialog()
-        return
-      }
-      const action = this.tweet.liked ? window.delete : window.post
-      action('/likes', {
-        likable_id: this.tweet.id,
-        likable_type: 'Tweet',
-      }).then(res => console.log('like/unlike succeed'))
-
-      this.tweet.liked = !this.tweet.liked
-      this.tweet.like_count += this.tweet.liked ? 1 : -1
-    },
-    confirmLogin() {
-      if (!this.currentUser) this.showLoginDialog()
-    },
-    showLoginDialog() {
-      this.$modal.show(LoginDialog, {}, { height: 'auto', width: 400 })
-    },
-    markdown(text) {
-      return marked(DOMPurify.sanitize(text))
+    markdown,
+    scaledAvatar,
+    noop() {},
+    shareTweet(tweet) {
+      setTimeout(() => {
+        this.sharing_tweet = tweet //
+        this.$modal.show('share-tweet')
+      }, 100)
     },
   },
 }
