dd1fcf480ecd668d68f5dcb860f02a6d4f949b36
ChrisZou <zoumengdie@gmail.com>
2021-03-28 11:53:00 +0800

    feat: cap 3.16.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,19 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "~> 3.16.0"
 
 set :application, "geekweibo"
 set :repo_url, "git@github.com:ChrisZou/geekweibo.git"
 set :deploy_to, "/var/www/#{fetch(:application)}"
 
-set :pty,             true
+set :pty, true
 
-set :user,            'chris'
-set :puma_workers,    0
-set :puma_bind,       "unix:///var/run/rails.#{fetch(:application)}.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :user, "chris"
+set :puma_workers, 0
+set :puma_bind, "unix:///var/run/rails.#{fetch(:application)}.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_error_log, "#{release_path}/log/puma.access.log"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true # Change to true if using ActiveRecord
@@ -27,38 +27,37 @@ set :migration_role, :db
 set :conditionally_migrate, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml'
-append :linked_files, 'config/master.key'
-append :linked_files, 'localstorage.store'
+append :linked_files, "config/database.yml"
+append :linked_files, "config/master.key"
+append :linked_files, "localstorage.store"
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/packs', 'node_modules', '.bundle'
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/packs", "node_modules", ".bundle"
 
 # Default value for default_env is {}
-set :default_env, PATH: '$PATH:/home/linuxbrew/.linuxbrew/bin:/opt/ruby/bin', 'NODE_ENV' => 'production'
-
+set :default_env, PATH: "$PATH:/home/linuxbrew/.linuxbrew/bin:/opt/ruby/bin", "NODE_ENV" => "production"
 
 namespace :deploy do
-  desc 'Make sure local git is in sync with remote.'
+  desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts 'WARNING: HEAD is not the same as origin/master'
-        puts 'Run `git push` to sync changes.'
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
         exit
       end
     end
   end
 
-  desc 'Initial Deploy'
+  desc "Initial Deploy"
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
+      before "deploy:restart", "puma:start"
+      invoke "deploy"
     end
   end
 
-  desc 'Create Directories for Puma Pids and Socket'
+  desc "Create Directories for Puma Pids and Socket"
   task :make_dirs do
     on roles(:app) do
       execute "mkdir #{shared_path}/tmp/sockets -p"
@@ -66,18 +65,18 @@ namespace :deploy do
     end
   end
 
-  desc 'Restart application'
+  desc "Restart application"
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke! 'puma:restart'
+      invoke! "puma:restart"
     end
   end
 
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :make_dirs
-  after  :finishing,    :restart
+  before :starting, :check_revision
+  after :finishing, :compile_assets
+  after :finishing, :cleanup
+  after :finishing, :make_dirs
+  after :finishing, :restart
 end
 
 # Capistrano 3
